86e87353c2f67f6f76c0a5b770c6c27b3e90aef4
Megan Collier <meganraec@live.com>
2018-05-06 23:21:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+#match will enumerate over the array of words given, and select all of the words that are an anagram from that set, returning an array of the matching words.
+  def match(array_of_words)
+    array_of_words.select do |anagram|
+      anagram?(anagram)
+    end
+    end
+
+  def anagram?(word)
+    word.chars.sort == @word.chars.sort
+  end
+
+end
