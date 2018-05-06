e04e24a57c4c654927d2df9383aadff5f02b2852
Megan Collier <meganraec@live.com>
2018-05-06 22:36:06 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_words)
+    letters = @word.split(/\s*/).sort
+    array_of_words.each do |matched_word|
+      matched_word.split(/\s*/).sort
+      matched_word == letters
+      matched_word ||= []
+    end
+  end
+
+end
