f7f65088881b56b4c93567cd5a49b4a3b5a29411
Megan Collier <meganraec@live.com>
2018-05-06 23:19:58 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,12 +6,18 @@ class Anagram
   end
 
   def match(array_of_words)
-    letters = @word.split(/\s*/).sort
-    array_of_words.each do |matched_word|
-      matched_word.split(/\s*/).sort
-      matched_word == letters
-      matched_word ||= []
+    array_of_words.select do |anagram|
+      anagram?(anagram)
     end
+    # # letters = @word.split(/\s*/).sort
+    # # array_of_words.each do |matched_word|
+    # #   matched_word.split(/\s*/).sort
+    # #   matched_word == letters
+    # #   matched_word ||= []
+    # end
+
+  def anagram?(word)
+    word.chars.sort == @word.chars.sort
   end
 
 end
