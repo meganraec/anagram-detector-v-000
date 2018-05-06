c81deec61c94e0c81ef03ed60700231c10802a1c
Megan Collier <meganraec@live.com>
2018-05-06 23:21:32 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,9 +5,11 @@ class Anagram
     @word = word
   end
 
+#match will enumerate over the array of words given, and select all of the words that are an anagram from that set, returning an array of the matching words. 
   def match(array_of_words)
     array_of_words.select do |anagram|
       anagram?(anagram)
+    end
     end
     # # letters = @word.split(/\s*/).sort
     # # array_of_words.each do |matched_word|
